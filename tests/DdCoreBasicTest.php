2d038fd6866aee16b0d2d166728a3a5d1ba34674
Beau Simensen <beau@dflydev.com>
2010-11-08 11:20:28 -0800

    More Phing integration, coverage. refs #39

--- tests/DdCoreBasicTest.php
+++ tests/DdCoreBasicTest.php
@@ -2,12 +2,16 @@
 
 require_once('PHPUnit/Framework.php');
 
+define('PACKAGE_LIB', dirname(dirname(__FILE__)) . '/lib/');
+
 class DdCoreBasicTest extends PHPUnit_Framework_TestCase {
 
     /**
      * Simple test to see if test starts up.
      */
     public function testStartup() {
+        require_once(PACKAGE_LIB . 'dd_core_ClasspathResourceLocator.php');
+        $resourceLocator = new dd_core_ClasspathResourceLocator();
     }
 
 }
