bb086d74af9a56f8e100fc7ebf499607e82dee08
Beau Simensen <beau@dflydev.com>
2010-11-08 00:44:37 -0800

    Testing stubs. refs #39

---
+++ tests/DdCoreBasicTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+
+class DdCoreBasicTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Simple test to see if test starts up.
+     */
+    public function testStartup() {
+    }
+
+}
+?>
